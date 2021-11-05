4e27181997e8f638e1aa1059f5bb3b2a19a01391
Peter Gastinger <peter@secitec.net>
2021-11-05 22:28:58 +0000

    added makemigrations

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -18,6 +18,7 @@ done
 sleep 2
 >&2 echo "REDIS is up - executing command"
 
+python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 exec "$@"
